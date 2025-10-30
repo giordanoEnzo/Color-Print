83ff1d02c3727285eb64f43f4a3ac41734dd7c2f
Ramirez Stwart <ramirezstwart@gmail.com>
2025-10-30 08:25:29 -0300

    Ajuste-ip

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ import packageInfo from '../../package.json';
 export const environment = {
   appVersion: packageInfo.version,
   production: true,
-  apiUrl: 'http://192.168.99.100:5000/api',      // para chamadas da API
-  assetsUrl: 'http://192.168.99.100:5000'        // para imagens e arquivos estáticos
+  apiUrl: 'http://192.168.99.101:5000/api',      // para chamadas da API
+  assetsUrl: 'http://192.168.99.101:5000'        // para imagens e arquivos estáticos
 };
