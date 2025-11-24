269170082aa5acfc8c9b8116681413e8b72b2708
Ramirez Stwart <ramirezstwart@gmail.com>
2025-11-24 07:33:17 -0300

    Rotas

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@ export const environment = {
   appVersion: packageInfo.version,
   production: true,
   apiUrl: 'https://backend.haretable.com.br/api',      // para chamadas da API
-  assetsUrl: 'https://backend.haretable.com.br'        // para imagens e arquivos estáticos
+  assetsUrl: 'https://backend.haretable.com.br/api'        // para imagens e arquivos estáticos
 };
 
 
